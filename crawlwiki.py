b08dd2355e022bc4c5501a11fd7007e349538d52
kyubin <kyubin@freshworks.io>
2017-10-17 08:47:30 -0700

    error handling is done for crawlwiki.py

--- crawlwiki.py
+++ crawlwiki.py
@@ -1,25 +1,35 @@
 import argparse
 import requests
+import sys
 
 def main(args):
-    isCaseSensitive = args.isCaseSensitive
+    is_case_sensitive = args.is_case_sensitive
     website_link = args.website_link if args.website_link != None else 'https://en.wikipedia.org/wiki/Oktoberfest' 
     word = args.word
 
-    # TODO: error handling
-    req = requests.get(website_link)
-    
-    if(isCaseSensitive):
+    # print is_case_sensitive, website_link, word
+
+    try:
+        req = requests.get(website_link)
+
+        word_count = get_occurrences(is_case_sensitive, req, word)
+        print 'The word "{0}" occurs {1} times on {2}'.format(word, word_count, website_link) 
+
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        print e
+        sys.exit(1)
+
+def get_occurrences(is_case_sensitive, req, word):
+    if(is_case_sensitive):
         # Return the number of (non-overlapping) occurrences of substring in the string. 
-        print req.text.count(word)
+        return req.text.count(word)
 
     # case-insentive search
     else: 
         lowercased_text = req.text.lower()
         lowercased_word = word.lower()
-        print lowercased_text.count(lowercased_word)
-    
-    print isCaseSensitive, website_link, word
+        return lowercased_text.count(lowercased_word)
 
 
 if __name__ == '__main__':
@@ -32,8 +42,8 @@ if __name__ == '__main__':
     parser.add_argument('word', action='store', help='a target word')
 
     parser.add_argument('-c', action='store_true', default = False,
-                    dest='isCaseSensitive', 
-                    help='set to true for a case-sensitive search')
+                    dest='is_case_sensitive', 
+                    help='set true for a case-sensitive search')
 
     args = parser.parse_args()
 
